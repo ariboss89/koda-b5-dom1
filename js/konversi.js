5728c0eb1ac68111242346382b5a0679b11b3acd
ariboss89 <ariboss89@gmail.com>
2025-11-25 21:57:48 +0700

    feat: adding reamur formula

---
+++ js/konversi.js
@@ -0,0 +1,36 @@
+const form = document.querySelector("main > form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const mySpan = document.getElementById("spanId");
+  const suhuCelcius = parseInt(event.target.suhuCelcius.value);
+  const lblReamur = document.getElementById("suhuReamur");
+  const lblFahrenheit = document.getElementById("suhuFahrenheit");
+  const lblKelvin = document.getElementById("suhuKelvin");
+
+  console.log(mySpan, "spannya");
+
+  let msg =
+    suhuCelcius == 0 || suhuCelcius == ""
+      ? "Silahkan input suhu !"
+      : suhuCelcius;
+
+  if (msg == "Silahkan input suhu !") {
+    mySpan.style.display = "block";
+    lblReamur = 0;
+    lblFahrenheit = 0;
+    lblKelvin = 0;
+  } else {
+    mySpan.style.display = "none";
+
+    const suhuReamur = (4 / 5) * suhuCelcius;
+    lblReamur.textContent = suhuReamur;
+
+    // const suhuFahrenheit = (9 / 5) * suhuCelcius + 32;
+    // lblFahrenheit.innerText = suhuFahrenheit;
+
+    // const suhuKelvin = suhuCelcius + 273.15;
+    // lblKelvin.textContent = suhuKelvin;
+  }
+});
